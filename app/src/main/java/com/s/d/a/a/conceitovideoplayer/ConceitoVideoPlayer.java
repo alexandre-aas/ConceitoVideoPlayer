c847feb68383928e62f4bcd29559f2c452b6a9bd
Alexandre <aas@unochapeco.edu.br>
2019-02-08 10:21:59 -0200

    Inclusão dos fontes do projeto

---
+++ app/src/main/java/com/s/d/a/a/conceitovideoplayer/ConceitoVideoPlayer.java
@@ -0,0 +1,13 @@
+package com.s.d.a.a.conceitovideoplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConceitoVideoPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conceito_video_player);
+    }
+}
